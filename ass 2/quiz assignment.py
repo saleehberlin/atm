d0f028b837a958d9d50d97a2b0144a9d59d39932
saleehberlin <berlinsaleeh@gmail.com>
2025-02-25 20:26:29 +0300

    Add files via upload

---
+++ ass 2/quiz assignment.py
@@ -0,0 +1,185 @@
+name =input("hello there ! what is your name? \n >>");
+print(f" Welcome {name} to quiz game \n" );
+score = 0
+answer=input(f"1. Where is mount Elgon found? \n >>").lower()
+
+if answer=="border of kenya uganda"or "kenya and uganda":
+    print("you've scored!\n")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 2. What is the difference time zone of WAT and EAT\n >>").lower()
+if input==" 2 hrs" or "2":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+      
+input(f" 3. In which year did kenya get independence \n >>").lower()
+if input=="1963":
+    print("you've scored!")
+    score=  score + 1
+    
+else:
+    print("failed");
+        
+input(f" 4.Whic lake borders 3 countries in eastern afica \n >>").lower()
+if input=="lake victoria" or"victoria":
+    print("you've scored!")
+    score=  score + 
+else:
+    print("failed");
+        
+input(f"5. which year was python language launched \n >>").lower()
+if input=="border of Kenya and uganda" :
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 6. in what form is data exchanged over the network \n >>").lower()
+if input=="packets":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 7. Which is the common language used to develop the front end part? \n >>").lower()
+if input=="html":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 8.Which is the longest river in africa? \n >>").lower()
+if input=="river nile" or"nile":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 9.how do we write thank you in german language? \n >>").lower()
+if input=="danke":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 10.Which tages ar used to input data rom user? \n >>").lower()
+if input=="input tags":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 11. How is javascript code included in html? \n >>").lower()
+if input=="script tags":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 12. what are the symbols used to comment python codes? \n >>").lower()
+if input=="triple quatation":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 13.what is the work of modulus operator? \n >>").lower()
+if input=="return remainder" or"results to remainder":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+        
+input(f" 14.what does CSS stand for? \n >>").lower()
+if input=="cascading style sheet":
+    print("you've scored!")
+    score=  score + 11
+else:
+    print("failed");
+input(f" 15. what is the capital city of kenya \n >>").lower()
+if input=="nairobi":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+
+input(f" 15. what sign is used in concatenation\n >>").lower()
+if input=="plus sign"or"+":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+    
+input(f" 16. (3*6+16/4-2) \n >>").lower()
+if input=="20":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+    
+input(f"17. In, falls under which type of operator \n>>").lower()
+if input=="membership operator" or"membership":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+    
+input(f" 18. name two type of operators >>").lower()
+if input=="assignment and precedence":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+    
+input(f" 19.Which is the largest ocean on earth >>").lower()
+if input=="pacific":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+    
+input(f" 20.isn orange a color or fruit \n >>").lower()
+if input=="fruit" or "color":
+    print("you've scored!")
+    score=  score + 1
+else:
+    print("failed");
+
+print("\n you scored",score,"out of 20!")    
+    
+
+
+    #score grading
+if score>=18:
+    grade="A1"
+    print("excellent! You {name} got {grade}!")
+elif score>=16:
+    grade="B2"
+    print("great job {name}! you got {grade}")
+elif score>=14:
+    grade="B3"
+    print("good job {name}! you got {grade}")
+elif score>=12:
+    grade="C4"
+    print("nice {name}! you got {grade}")
+elif score>=10:
+    grade="C5"
+    print("wonderful {name}! you got {grade}")
+elif score>=8:
+    grade="C6"
+    print("superb {name}! you got {grade}")
+elif score>=6:
+    grade="D7"
+    print("doing great {name}! you got {grade}")
+elif score>=4:
+    grade="E8"
+    print("nice trial {name}! you got {grade}")
+    
+    
+        
+
+
